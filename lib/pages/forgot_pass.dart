1412c2369c9a75d003135065f8e2d8a55a4cb0bf
John-Liton-Mardy <mardy.ces@gmail.com>
2023-10-15 22:54:11 +0600

    forget pass done

---
+++ lib/pages/forgot_pass.dart
@@ -0,0 +1,42 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+class ForgotPass extends StatefulWidget {
+  const ForgotPass({super.key});
+
+  @override
+  State<ForgotPass> createState() => _ForgotPassState();
+}
+
+class _ForgotPassState extends State<ForgotPass> {
+  final emailController = TextEditingController();
+  final auth = FirebaseAuth.instance;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Forgot Pass"),
+      ),
+      body: Column(
+        children: [
+          TextFormField(
+            controller: emailController,
+            decoration: InputDecoration(
+              hintText: "Email"
+            ),
+          ),
+          SizedBox(height: 40,),
+          ElevatedButton(onPressed: (){
+
+            auth.sendPasswordResetEmail(email: emailController.text.toString()).then((value){
+
+            }).onError((error, stackTrace){
+
+            });
+          }, child: Text("Reset Pass"))
+        ],
+      )
+    );
+  }
+}
